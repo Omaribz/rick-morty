e166292a079df082f3a9cc4d9f760a54d532483c
Omaribz <cynthiamyud@gmail.com>
2024-03-16 16:40:15 +0300

    Create a custom hook to fetch the characters

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,51 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface Character {
-  id: number;
-  name: string;
-  status: string;
-  species: string;
-  type: string | null;
-  gender: string;
-  origin: {
-    name: string;
-    url: string;
-  };
-  location: {
-    name: string;
-    url: string;
-  };
-  image: string;
-  episode: string[];
-  url: string;
-  created: string;
-}
-
-interface Info {
-  count: number;
-  pages: number;
-  next: string | null;
-  prev: string | null;
-}
-
-interface CharacterResponse {
-  info: Info;
-  results: Character[];
-}
+import useCharacters from "../hooks/useCharacters";
 
 const Characters = () => {
-  const [characters, setCharacters] = useState<Character[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<CharacterResponse>("/character")
-      .then((res) => setCharacters(res.data.results))
-      .catch((err) => setError(err.message));
-  });
-
+  const { characters, error } = useCharacters();
   return (
     <>
       {error && <Text>{error}</Text>}
---
+++ src/hooks/useCharacters.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react'
+import apiClient from '../services/api-client';
+
+interface Character {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string | null;
+    gender: string;
+    origin: {
+      name: string;
+      url: string;
+    };
+    location: {
+      name: string;
+      url: string;
+    };
+    image: string;
+    episode: string[];
+    url: string;
+    created: string;
+  }
+  
+  interface Info {
+    count: number;
+    pages: number;
+    next: string | null;
+    prev: string | null;
+  }
+  
+  interface CharacterResponse {
+    info: Info;
+    results: Character[];
+  }
+
+const useCharacters = () => {
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    apiClient
+      .get<CharacterResponse>("/character")
+      .then((res) => setCharacters(res.data.results))
+      .catch((err) => setError(err.message));
+  }, []);
+
+  return { characters, error};
+}
+
+export default useCharacters
